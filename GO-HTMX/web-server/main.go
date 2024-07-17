fdbac077cdb9ac9bcd882669dab7839acc10d6eb
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-07-17 10:23:42 +0530

    completeed the webserver with go + htmx

--- GO-HTMX/web-server/main.go
+++ GO-HTMX/web-server/main.go
@@ -5,20 +5,20 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
-	Title string
+	Title    string
 	Director string
-
 }
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Opening App...")
 
 	// writingh to the web server
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		
+
 		// to send a direct message
 		// io.WriteString(w, "Hello world/n")
 		// io.WriteString(w, r.Method)
@@ -35,8 +35,18 @@ func main() {
 		tmpl.Execute(w, films)
 	}
 
+	// returns the template block with the newly added file as an HTMX response
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+
 	// handling requests
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
